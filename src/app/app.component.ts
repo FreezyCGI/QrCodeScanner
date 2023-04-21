ae4898d12beff2d65b7db3db795c6e4fde7f3223
FreezyCGI <patrick.fritzl@gmx.at>
2023-04-21 18:23:55 +0200

    Added text recognition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,32 @@
 import { Component, HostListener, NgZone } from '@angular/core';
 import { WebcamImage } from 'ngx-webcam';
 import QrScanner from 'qr-scanner';
-import { Observable, Subject, Subscriber } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { Tesseract } from 'tesseract.ts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent
-{
+export class AppComponent {
   public webCamResult?: QrScanner.ScanResult;
   private trigger: Subject<void> = new Subject<void>();
   public mousePos: { x: number, y: number } = { x: 0, y: 0 };
   public webcamEnabled = true;
+  public recognizedText: Tesseract.Line[] = [];
+  public webCamImageAsUrl = "";
+
+  public mediaTrackConstraints: MediaTrackConstraints = { width: { min: 1920 }, height: { min: 1080 } }
 
   constructor(private zone: NgZone) { }
 
-  ngOnInit()
-  {
+  ngOnInit() {
 
-    this.zone.runOutsideAngular(() =>
-    {
+    this.zone.runOutsideAngular(() => {
       navigator.mediaDevices.getUserMedia({ video: true })
-        .then(stream =>
-        {
-          this.zone.run(() =>
-          {
+        .then(stream => {
+          this.zone.run(() => {
             console.log(stream)
             // this.mediaTrackConstraints.deviceId = stream.id;
             // this.switchCameraSubscriber?.next(stream.id);
@@ -34,26 +34,34 @@ export class AppComponent
         })
         .catch(err => console.error(err));
     })
-
   }
 
-  @HostListener('mousemove', ['$event']) onMouseMove(event: any)
-  {
+  @HostListener('mousemove', ['$event']) onMouseMove(event: any) {
     //console.log(event.clientX, event.clientY);
     this.mousePos.x = event.clientX;
     this.mousePos.y = event.clientY;
   }
 
-  public triggerSnapshot(): void
-  {
+  public triggerSnapshot(): void {
     this.trigger.next();
   }
 
-  public async handleImage(webcamImage: WebcamImage): Promise<void>
-  {
+  public async handleImage(webcamImage: WebcamImage): Promise<void> {
+    this.webCamImageAsUrl = webcamImage.imageAsDataUrl;
+
+    Tesseract.create()
+
+    Tesseract
+      .recognize(webcamImage.imageAsDataUrl, {tessedit_write_images:true})
+      .progress(console.log)
+      .then((res: Tesseract.Page) => {
+        console.log(res);
+        this.recognizedText = res.lines
+        
+      })
+      .catch(console.error);
     let result = await QrScanner.scanImage(webcamImage.imageAsDataUrl, { returnDetailedScanResult: true })
-      .catch((err) =>
-      {
+      .catch((err) => {
         console.log(err)
       });
 
@@ -62,10 +70,12 @@ export class AppComponent
 
     this.webCamResult = result;
     console.log(result)
+
+
   }
 
-  public get triggerObservable(): Observable<void>
-  {
+  public get triggerObservable(): Observable<void> {
     return this.trigger.asObservable();
   }
 }
+
