57d7b518f037509fc79311e0797a37fab3dcd65f
FreezyCGI <patrick.fritzl@gmx.at>
2023-04-21 18:53:26 +0200

    added PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, HostListener, NgZone } from '@angular/core';
 import { WebcamImage } from 'ngx-webcam';
 import QrScanner from 'qr-scanner';
-import { Observable, Subject } from 'rxjs';
+import { interval, Observable, scan, Subject } from 'rxjs';
 import { Tesseract } from 'tesseract.ts';
 
 @Component({
@@ -22,24 +22,31 @@ export class AppComponent {
   constructor(private zone: NgZone) { }
 
   ngOnInit() {
+    // const source = interval(500);
+    // source.subscribe(val => this.triggerSnapshot());
 
-    this.zone.runOutsideAngular(() => {
-      navigator.mediaDevices.getUserMedia({ video: true })
-        .then(stream => {
-          this.zone.run(() => {
-            console.log(stream)
-            // this.mediaTrackConstraints.deviceId = stream.id;
-            // this.switchCameraSubscriber?.next(stream.id);
-          });
-        })
-        .catch(err => console.error(err));
-    })
+    // this.zone.runOutsideAngular(() => {
+    //   navigator.mediaDevices.getUserMedia({ video: true })
+    //     .then(stream => {
+    //       this.zone.run(() => {
+    //         console.log(stream)
+    //         // this.mediaTrackConstraints.deviceId = stream.id;
+    //         // this.switchCameraSubscriber?.next(stream.id);
+    //       });
+    //     })
+    //     .catch(err => console.error(err));
+    // })
   }
 
-  @HostListener('mousemove', ['$event']) onMouseMove(event: any) {
-    //console.log(event.clientX, event.clientY);
-    this.mousePos.x = event.clientX;
-    this.mousePos.y = event.clientY;
+  // @HostListener('mousemove', ['$event']) onMouseMove(event: any) {
+  //   //console.log(event.clientX, event.clientY);
+  //   this.mousePos.x = event.clientX;
+  //   this.mousePos.y = event.clientY;
+  // }
+
+  mouseMoved($event: any) {
+    this.mousePos.x = $event.clientX;
+    this.mousePos.y = $event.clientY;
   }
 
   public triggerSnapshot(): void {
@@ -48,30 +55,50 @@ export class AppComponent {
 
   public async handleImage(webcamImage: WebcamImage): Promise<void> {
     this.webCamImageAsUrl = webcamImage.imageAsDataUrl;
+    this.ScanForQrCode(webcamImage);
+    this.ScanForText(webcamImage);
+  }
 
-    Tesseract.create()
+  scanForTextInProgress = false;
+  scanForQrCodeInProgress = false;
 
-    Tesseract
-      .recognize(webcamImage.imageAsDataUrl, {tessedit_write_images:true})
+  ScanForText(webcamImage: WebcamImage) {
+    if (this.scanForTextInProgress)
+      return;
+
+    this.scanForTextInProgress = true;
+
+    let t = Tesseract
+      .recognize(webcamImage.imageAsDataUrl, { tessedit_write_images: true })
       .progress(console.log)
       .then((res: Tesseract.Page) => {
         console.log(res);
         this.recognizedText = res.lines
-        
       })
-      .catch(console.error);
+      .catch(console.error)
+      .finally(() => {
+        this.scanForTextInProgress = false;
+      })
+  }
+
+  async ScanForQrCode(webcamImage: WebcamImage) {
+    if (this.scanForQrCodeInProgress)
+      return;
+
+    this.scanForQrCodeInProgress = true;
+
     let result = await QrScanner.scanImage(webcamImage.imageAsDataUrl, { returnDetailedScanResult: true })
       .catch((err) => {
         console.log(err)
-      });
+      }).finally(() => {
+        this.scanForQrCodeInProgress = false;
+      })
 
     if (!result)
       return;
 
     this.webCamResult = result;
     console.log(result)
-
-
   }
 
   public get triggerObservable(): Observable<void> {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WebcamModule } from 'ngx-webcam';
-
+import { ButtonModule } from 'primeng/button';
+import { PanelModule } from 'primeng/panel';
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +13,10 @@ import { WebcamModule } from 'ngx-webcam';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    WebcamModule
+    WebcamModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    PanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
